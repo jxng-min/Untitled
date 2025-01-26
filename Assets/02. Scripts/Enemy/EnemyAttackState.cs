0756de31ab8c14ddddcd8d90cdd35f5b34962636
wnsdud3095 <wnsdud30954@naver.com>
2025-01-27 02:39:25 +0900

    feat : BattleAxeCtrl작성, 정 공격방식 변경

--- Assets/02. Scripts/Enemy/EnemyAttackState.cs
+++ Assets/02. Scripts/Enemy/EnemyAttackState.cs
@@ -6,19 +6,35 @@ namespace Junyoung
     public class EnemyAttackState : MonoBehaviour,IEnemyState<EnemyCtrl>
     {
         private EnemyCtrl m_enemy_ctrl;
-        private PlayerCtrl m_player;
+        private PlayerCtrl m_player_ctrl;
         private float m_atk_ani_length = 1f;
+        private bool m_is_hitting;
+
         public void OnStateEnter(EnemyCtrl sender)
         {
-            m_enemy_ctrl = sender;
+            if(m_enemy_ctrl==null)
+            {
+                m_enemy_ctrl = sender;
+
+                GameObject[] m_players = GameObject.FindGameObjectsWithTag("Player");
+
+                foreach(GameObject p in m_players ) // Player태그를 가진 최상위 오브젝트를 찾음
+                {
+                    if (p.transform.parent == null)
+                    {
+                        m_player_ctrl = p.GetComponent<PlayerCtrl>();
+                    }
+                }
+
+            }
             m_enemy_ctrl.Animator.SetTrigger("Attack");
-            m_player = gameObject.GetComponent<PlayerCtrl>();
-                                 
+            m_is_hitting = false;
             StartCoroutine(GetAniLength());
             m_enemy_ctrl.AttackDelay = 0;
 
-            //m_player.GetDamage(m_enemy_ctrl.EnemyStat.AtkDamege);
-            Debug.Log($"플레이어가{m_enemy_ctrl.EnemyStat.AtkDamege}의 데미지 입음");
+
+
+            //Debug.Log($"플레이어가{m_enemy_ctrl.EnemyStat.AtkDamege}의 데미지 입음");
         }
         public void OnStateUpdate(EnemyCtrl sender)
         {
@@ -30,9 +46,16 @@ namespace Junyoung
             {
                 m_enemy_ctrl.ChangeState(EnemyState.READY);
             }
+
+            if (m_enemy_ctrl.IsHit && m_player_ctrl != null && !m_is_hitting)
+            {
+                m_player_ctrl.GetDamage(m_enemy_ctrl.EnemyStat.AtkDamege);
+                m_is_hitting = true;
+            }
         }
         public void OnStateExit(EnemyCtrl sender)
         {
+            m_enemy_ctrl.IsHit = false;
         }
 
         private IEnumerator GetAniLength() // Attack 트리거가 호출 됐지만 딜레이가 있어서 StateInfo가 애니메이션 전환 전에 호출되는 문제 때문에 사용
