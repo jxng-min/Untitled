63d99bc2825aee126c15b1036a41bd5140cce0ee
wnsdud3095 <wnsdud30954@naver.com>
2025-02-11 23:19:13 +0900

    feat : ArrowCtrl, ArrowFactory 작성

---
+++ Assets/02. Scripts/Weapon/EnemyWeapon/ArrowCtrl.cs
@@ -0,0 +1,36 @@
+using Junyoung;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Junyoung
+{
+    public class ArrowCtrl : EnemyWeaponCtrl
+    {
+        [SerializeField]
+        private Transform m_arrow_transfrom;
+
+        public IObjectPool<ArrowCtrl> ManagedArrowPool { get; set; }
+
+
+        public void SetArrowPool(IObjectPool<ArrowCtrl> pool)
+        {
+            ManagedArrowPool = pool;
+        }
+
+        public void ReturnToPool()
+        {
+            ManagedArrowPool.Release(this);
+        }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
