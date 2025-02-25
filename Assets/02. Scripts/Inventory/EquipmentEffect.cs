e3e66e221c612cd938eab0cdb92a34f287d90023
Kim Jongmin <vwmartin@naver.com>
2025-02-25 23:33:12 +0900

    feat: In Game Setting UI 추가

--- Assets/02. Scripts/Inventory/EquipmentEffect.cs
+++ Assets/02. Scripts/Inventory/EquipmentEffect.cs
@@ -35,15 +35,3 @@ using UnityEngine;
         return calculated_effect;
     }
 }
-
-[CreateAssetMenu(fileName ="Equipment Item", menuName ="Scriptable Object/Item(Equipment)")]
-public class Item_Equipment : Item
-{
-    [Space(50)] [Header("장비 아이템의 효과 (착용 시에 발생)")]
-    [SerializeField] private EquipmentEffect m_effect;
-
-    public EquipmentEffect Effect
-    {
-        get { return m_effect; }
-    }
-}
